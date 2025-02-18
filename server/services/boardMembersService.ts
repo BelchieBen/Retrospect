8453d761b913fd61cdaaf4898886da799c89aecf
Martin Liggett <martin.liggett@ideagenplc.com>
2025-02-18 14:23:31 +0000

    Added Board Members service

---
+++ server/services/boardMembersService.ts
@@ -0,0 +1,28 @@
+export default class BoardMembersService {
+  id: string;
+
+  constructor(id: string) {
+    this.id = id;
+  }
+
+  async addToBoard(id: string, boardId: string) {
+    try {
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async removeFromBoard(id: string, boardId: string) {
+    try {
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async findById(id: string, boardId: string) {
+    try {
+    } catch (error) {
+      throw error;
+    }
+  }
+}
