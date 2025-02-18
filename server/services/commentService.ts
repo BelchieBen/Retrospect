87a89a69ea165cc760c5d4a50c1ac988f1d3f29b
Martin Liggett <martin.liggett@ideagenplc.com>
2025-02-18 15:05:38 +0000

    Added Comment Service

---
+++ server/services/commentService.ts
@@ -0,0 +1,49 @@
+export default class CardService {
+  id: string;
+
+  constructor(id: string) {
+    this.id = id;
+  }
+
+  async create(data: any) {
+    try {
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async update(id: string, data: any) {
+    try {
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async remove(id: string) {
+    try {
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async destroyAll(ids: string[]) {
+    try {
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async findById(id: string) {
+    try {
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async rearrange(id: string, order: number) {
+    try {
+    } catch (error) {
+      throw error;
+    }
+  }
+}
