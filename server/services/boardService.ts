e5e203c8b8f4cb919e60e368ed1b914c5f0a81f3
Martin Liggett <martin.liggett@ideagenplc.com>
2025-02-18 11:02:10 +0000

    Change Board Service to Typescript

---
+++ server/services/boardService.ts
@@ -0,0 +1,46 @@
+export default class BoardService {
+  id: string;
+
+  constructor(id: string) {
+    this.id = id;
+  }
+
+  async create(data: any) {
+    try {
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async update(id: string, data: any) {
+    try {
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async remove(id: string) {
+    try {
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async destroyAll(ids: string[]) {
+    try {
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async findById(id: string) {
+    try {
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async import(data: any) {
+    return this.create();
+  }
+}
