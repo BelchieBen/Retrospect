124e600ef85a874e640107ac589cf0b29ea2a5ab
Ferhan Ali <ferhan.ali@ideagen.com>
2025-02-19 15:04:06 +0000

    update components

--- frontend/src/app/demo/page.tsx
+++ frontend/src/app/demo/page.tsx
@@ -1,11 +1,14 @@
+import { SimpleCard } from "~/components/cardForm";
 import { AddPostForm } from "../_components/post-form";
 import Posts from "../_components/realtime-post";
 
-export default function RealtimeDemoPage(){
-    return (
-        <>
-        <AddPostForm />
-        <Posts />
-        </>
-    )
+export default function RealtimeDemoPage() {
+  return (
+    <>
+      <AddPostForm />
+      <Posts />
+      <SimpleCard />
+      <SimpleCard />
+    </>
+  );
 }
---
+++ frontend/src/components/avatar.tsx
@@ -0,0 +1,21 @@
+"use client";
+import { useSession } from "next-auth/react";
+import * as React from "react";
+import { Avatar, AvatarImage, AvatarFallback } from "~/components/ui/avatar";
+
+export const Avatars = () => {
+  const { data: session } = useSession();
+  return (
+    <div className="flex items-center justify-end">
+      <div className="flex items-center justify-start">
+        <Avatar>
+          <AvatarImage
+            src={session?.user?.image ?? ""}
+            alt={session?.user?.name ?? ""}
+          />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+      </div>
+    </div>
+  );
+};
